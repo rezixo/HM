62e55879d9fd3ca8de0bf46fb8bbe97646dee0b5
Hamidreza Mirhosseini <hamidrezamirhosseini33@gmail.com>
2025-01-29 22:55:45 +0330

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("سیستم‌عامل HM تحت وب بارگذاری شد!");
+    // سایر کدهای مربوط به تعاملات جاوا اسکریپت می‌توانند در اینجا قرار بگیرند
+});
