884e4dff5cd3ab3f3f3854391456ec16bf1d6d50
Hamidreza Mirhosseini <hamidrezamirhosseini33@gmail.com>
2025-01-30 00:06:07 +0330

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("سیستم‌عامل HM تحت وب بارگذاری شد!");
-    // سایر کدهای مربوط به تعاملات جاوا اسکریپت می‌توانند در اینجا قرار بگیرند
-});
